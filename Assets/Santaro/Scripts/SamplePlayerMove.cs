9a4719dbd91a9f114a13a39204228998f5b4e0c2
Papyrustaro <undertale1104@gmail.com>
2020-03-03 20:12:23 +0900

    ファイル共有テスト

---
+++ Assets/Santaro/Scripts/SamplePlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamplePlayerMove : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        this.transform.Translate(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    }
+}
